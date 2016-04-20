7e845c01c87c11dbd38f0f78b3c3b33aa6f0878c
Viacheslav Pradchuk <q>
2016-04-20 14:14:05 +0300

    Fixed task creation script

--- Scheduler/fetch_task_create.bat
+++ Scheduler/fetch_task_create.bat
@@ -1,1 +1,2 @@
-schtasks /Create /SC "DAILY" /TN "git_fetch" /TR "../Fetch.sh" /ST "10:00" /RI 5
+set currentDir=%~dp0
+schtasks /Create /SC "DAILY" /TN "git_fetch" /TR "%currentDir%..\Fetch.sh" /ST "10:00" /DU "10:00" /RI 5
