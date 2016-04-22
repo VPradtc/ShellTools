33ba993635cd687a13771b5735704497162fe576
Viacheslav Pradchuk <q>
2016-04-22 20:50:59 +0300

    Fixed fetch job setup.

--- Fetch.sh
+++ Fetch.sh
@@ -1,25 +1,33 @@
 #!/bin/sh
 ResFile="$0.res"
+CurrentDir="$1" || ""
 echo $'\n\n----' >> "${ResFile}"
 set -o pipefail
 set -uC
 ( (
 	set -ex
 	pwd
-	source './zConfig.sh'
+	source "${CurrentDir}./zConfig.sh"
+	WorkDir="${CurrentDir}${WorkDir}";
 	cd "${WorkDir}"
 
-    (
-	  ExitCode=0
-	  time git fetch --all --prune --verbose ||
-	  ExitCode=$?
-    ) ; (exit $?) || false
+	BrName='master'
+	FetchCmd="git fetch . refs/remotes/${RemoteBrName}:refs/heads/${BrName}"
+	${FetchCmd} ||
+	(
+		CurrentPos="$(git rev-parse --abbrev-ref HEAD)"
+		git checkout -q HEAD~0
+		ExitCode=0
+		${FetchCmd} ||
+		ExitCode=$?
+		git checkout -q "${CurrentPos}"
+		exit ${ExitCode}
+	) ; (exit $?) || false
 
   )
-  
-  ExitCode=$?
-  exit ${ExitCode}) && echo 'Success' || echo "Error / ExitCode = $?"
-  exit ${ExitCode}
+	ExitCode=$?
+	(exit ${ExitCode}) && echo 'Success' || echo "Error / ExitCode = $?"
+	exit ${ExitCode}
 ) 2>&1 | tee -a "${ResFile}"
-
+ExitCode=$?
 exit ${ExitCode}
--- Scheduler/fetch_task_create.bat
+++ Scheduler/fetch_task_create.bat
@@ -1,2 +1,2 @@
 set currentDir=%~dp0
-schtasks /Create /SC "DAILY" /TN "git_fetch" /TR "%currentDir%..\Fetch.sh" /ST "10:00" /DU "10:00" /RI 5
+schtasks /Create /SC "WEEKLY" /D "MON,TUE,WED,THU,FRI" /TN "git_fetch" /TR "%currentDir%..\Fetch.sh %currentDir%..\\" /ST "10:00" /DU "10:00" /RI 5
